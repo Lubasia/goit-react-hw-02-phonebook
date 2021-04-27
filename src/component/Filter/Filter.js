57414f021f576ef150a781ba9e0556df3929e48a
Lubasia <lubasia.oasis@gmail.com>
2021-04-27 17:33:35 +0300

    HW2- part2

---
+++ src/component/Filter/Filter.js
@@ -0,0 +1,14 @@
+const Filter = ({ filter, onFilterChange }) => {
+  const onChange = e => {
+    onFilterChange(e.target.value);
+  };
+
+  return (
+    <label>
+      Find contacts by name 🔍
+      <input type="text" value={filter} onChange={onChange} name="filter" />
+    </label>
+  );
+};
+
+export default Filter;
